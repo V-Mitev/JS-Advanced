6f309377321413807a54b3291eff4936f1db1f37
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-01-25 13:19:20 +0200

    Update task 1 and remove one file

--- Objects and Composition/Exercise/01.CalorieObject.js
+++ Objects and Composition/Exercise/01.CalorieObject.js
@@ -13,32 +13,5 @@ function calorieObject(input) {
     return result;
 }
 
-// console.log(calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
-// console.log(calorieObject(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']));
-
-// 02. Construction Crew
-
-function constructionCrew() {
-
-}
-
-console.log(constructionCrew({
-    weight: 80,
-    experience: 1,
-    levelOfHydrated: 0,
-    dizziness: true
-}));
-
-console.log(constructionCrew({
-    weight: 120,
-    experience: 20,
-    levelOfHydrated: 200,
-    dizziness: true
-}));
-
-console.log(constructionCrew({
-    weight: 95,
-    experience: 3,
-    levelOfHydrated: 0,
-    dizziness: false
-}));
+console.log(calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
+console.log(calorieObject(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']));
---
+++ Objects and Composition/Exercise/02.ConstructionCrew.js
@@ -0,0 +1,24 @@
+function solve() {
+
+}
+
+console.log(solve({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+}));
+
+console.log(solve({
+    weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true
+}));
+
+console.log(solve({
+    weight: 95,
+    experience: 3,
+    levelOfHydrated: 0,
+    dizziness: false
+}));
