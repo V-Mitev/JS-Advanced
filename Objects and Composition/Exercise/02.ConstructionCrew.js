165603238973932c21378cfcafb280573a611c28
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-01-26 21:29:59 +0200

    Solve 02.ConstructionCrew task

--- Objects and Composition/Exercise/02.ConstructionCrew.js
+++ Objects and Composition/Exercise/02.ConstructionCrew.js
@@ -1,5 +1,10 @@
-function solve() {
+function solve(worker) {
+    if (worker.dizziness) {
+        worker.levelOfHydrated += 0.1 * worker.weight * worker.experience;
+        worker.dizziness = false;
+    }
 
+    return worker;
 }
 
 console.log(solve({
