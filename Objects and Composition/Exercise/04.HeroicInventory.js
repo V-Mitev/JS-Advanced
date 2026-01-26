b6e84809f9d73aa8e5e539c51385c604197e3e32
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-01-26 21:59:46 +0200

    Solve 04.HeroicInventory task

---
+++ Objects and Composition/Exercise/04.HeroicInventory.js
@@ -0,0 +1,18 @@
+function registerHeroes(heroesDescription) {
+    const heroes = heroesDescription.map(hero => {
+        const [name, levelStr, items] = hero.split(' / ');
+        return {
+            name,
+            level: Number(levelStr),
+            items: items ? items.split(', ') : []
+        }
+    });
+
+    return JSON.stringify(heroes);
+}
+
+console.log(solve(['Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara']));
+
+console.log(solve(['Jake / 1000 / Gauss, HolidayGrenade']));
