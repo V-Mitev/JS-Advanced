f71618c31168373ebe7e85bbb73a559bb669126d
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-01-29 23:59:20 +0200

    Solve 02.CollectList task

--- DOM Introduction/Lab/01. Edit Element_Resources/02.Collect List Items/CollectListItems.js
+++ DOM Introduction/Lab/01. Edit Element_Resources/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,18 @@
 function extractText() {
-    // TODO
+    const result = document.getElementById('result');
+    const listItems = Array.from(document.getElementsByTagName('li'));
+
+    let listValues = [];
+    for (const list of listItems) {
+        listValues.push(list.textContent);
+    }
+
+    result.textContent = listValues.join('\n');
+}
+
+function extractText() {
+    const result = document.getElementById('result');
+    const listItems = Array.from(document.getElementsByTagName('li'));
+
+    result.textContent = listItems.map(item => item.textContent).join('\n');
 }
