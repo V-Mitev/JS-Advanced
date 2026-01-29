86f66a3398858121a83d6ab31932b7d803036919
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-01-29 23:23:35 +0200

    Solve 01.EditElement task

--- DOM Introduction/Lab/01. Edit Element_Resources/01.Edit Element/EditElement.js
+++ DOM Introduction/Lab/01. Edit Element_Resources/01.Edit Element/EditElement.js
@@ -1,3 +1,10 @@
-function editElement() {
-    // TODO
+function editElement(el, matcher, replacer) {
+    while (el.textContent.includes(matcher)) {
+        el.textContent = el.textContent.replace(matcher, replacer);
+    }
+}
+
+function editElement(el, matcher, replacer) {
+    let text = el.textContent;
+    el.textContent = text.split(matcher).join(replacer);
 }
