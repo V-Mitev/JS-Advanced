5a1b61abfec7592ce176b15a56f9ba9e0f953cff
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-01-30 00:37:46 +0200

    Solve 06.SumTable task

--- DOM Introduction/Lab/01. Edit Element_Resources/06.Sum Table/SumTable.js
+++ DOM Introduction/Lab/01. Edit Element_Resources/06.Sum Table/SumTable.js
@@ -1,3 +1,14 @@
 function sumTable() {
+    let result = 0;
+    const elements = document.getElementsByTagName('td');
 
+    for (const element of elements) {
+        const value = element.textContent;
+
+        if (!isNaN(value)) {
+            result += Number(value);
+        }
+    }
+
+    document.getElementById('sum').textContent = result;
 }
