87daf311c9cd8dd7f01076f8118b5ad72f3e3c60
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-01-30 01:03:51 +0200

    Solve  07.ExtractParenthesis task

--- DOM Introduction/Lab/01. Edit Element_Resources/07.Extract Parenthesis/ExtractParenthesis.js
+++ DOM Introduction/Lab/01. Edit Element_Resources/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
 function extract(content) {
+    const text = document.getElementById(content).textContent;
+    const matches = text.match(/\(([^)]+)\)/g);
 
+    if (!matches) {
+        return '';
+    }
+
+    return matches
+        .map(x => x.slice(1, -1))
+        .join('; ');
 }
