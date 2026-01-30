180773691f590c096d2cdba4ba7264333bb109e7
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-01-30 09:00:23 +0200

    Solve 02.PascalOrCamelCase task

--- DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
+++ DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-  //TODO...
+  let words = document.getElementById('text').value
+    .toLowerCase()
+    .split(' ');
+
+  const convention = document.getElementById('naming-convention').value;
+  const result = document.getElementById('result');
+
+  if (convention !== 'Camel Case' && convention !== 'Pascal Case') {
+    result.textContent = 'Error!';
+    return;
+  }
+
+  for (let i = 0; i < words.length; i++) {
+    if (i === 0 && convention === 'Camel Case') {
+      continue;
+    }
+
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+
+  result.textContent = words.join('');
 }
