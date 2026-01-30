95e8c507ad6853a167bbac728bbbf8673b8429f8
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-01-30 09:34:04 +0200

    Solve 04.SearchInList task

--- DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise/04. Search in List/search.js
+++ DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   let matches = 0;
+   const result = document.getElementById('result');
+   const towns = document.querySelectorAll('#towns>li');
+   const search = document.getElementById('searchText').value.toLowerCase();
+
+   for (const town of towns) {
+      town.style.fontWeight = '';
+      town.style.textDecoration = 'none';
+
+      if (search !== '' && town.textContent.toLowerCase().includes(search)) {
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+         matches++;
+      }
+   }
+
+   result.textContent = `${matches} matches found`;
 }
