20369969d1c47b473fbad7bbdb850118a5dab593
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-01-30 11:21:55 +0200

    Solve 05.Table-SearchEngine task

--- DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
+++ DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,21 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const input = document.getElementById('searchField');
+      const search = input.value.toLowerCase();
+      const rows = document.querySelectorAll('tbody > tr');
 
+      for (const row of rows) {
+         row.classList.remove('select');
+
+         for (const cell of row.children) {
+            if (search !== '' && cell.textContent.toLowerCase().includes(search)) {
+               row.classList.add('select');
+               break;
+            }
+         }
+      }
+
+      input.value = '';
    }
 }
