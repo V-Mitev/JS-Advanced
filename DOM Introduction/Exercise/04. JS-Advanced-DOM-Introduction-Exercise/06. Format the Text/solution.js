d6a02011daa4f511e13f206161e948477b6dc6c5
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-02-02 22:15:06 +0200

    Solve 06.FormatTheText task

--- DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise/06. Format the Text/solution.js
+++ DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise/06. Format the Text/solution.js
@@ -1,3 +1,38 @@
 function solve() {
-  //TODO
+  const textArea = document.getElementById('input');
+  const arr = textArea.value.split('.');
+  const result = document.getElementById('output');
+  result.textContent = '';
+
+  let counter = 0;
+  let sentences = '';
+  let sentencesCount = arr.length;
+
+  for (let i = 0; i < sentencesCount; i++) {
+    let sentence = arr[i].trim();
+
+    if (sentence.length < 1) {
+      continue;
+    }
+
+    counter++;
+    sentences += sentence + '. ';
+
+    if (counter === 3) {
+      const pElement = document.createElement('p');
+      pElement.textContent = sentences.trim();
+
+      result.appendChild(pElement);
+
+      counter = 0;
+      sentences = '';
+    }
+  }
+
+  if (sentences.length > 0) {
+    const pElement = document.createElement('p');
+    pElement.textContent = sentences.trim();
+
+    result.appendChild(pElement);
+  }
 }
