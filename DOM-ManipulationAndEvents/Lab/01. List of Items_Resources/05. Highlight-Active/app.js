9cfb4493ee51d62fc33b18a9549f41727b76ce2e
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-02-05 21:37:58 +0200

    Solve 05.HighlightActive task

--- DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/05. Highlight-Active/app.js
+++ DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+    const inputs = Array.from(document.querySelectorAll('div input'));
+
+    inputs.forEach(input => {
+        input.addEventListener("focus", onFocus, false);
+        input.addEventListener("blur", onBlur, false);
+    });
+
+    function onFocus(e) {
+        e.target.parentElement.classList.add('focused');
+    }
+
+    function onBlur(e) {
+        e.target.parentElement.classList.remove('focused');
+    }
 }
