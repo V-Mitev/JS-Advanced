7bc089e676d12719e9d9a55f0047450b1ee239cf
V-Mitev <vladimirmitev6969@gmail.com>
2026-02-05 11:44:04 +0200

    Solve 01.ListOfItems task

--- DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/01. List-Of-Items/app.js
+++ DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    const list = document.getElementById("items");
+    const inputValue = document.getElementById("newItemText").value;
+    
+    if (inputValue.trim() !== "") {
+        const newItem = document.createElement("li");
+        newItem.textContent = inputValue;
+        list.appendChild(newItem);
+        document.getElementById("newItemText").value = "";
+    }
 }
