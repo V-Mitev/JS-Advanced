59cf9da72d6591116ec00cbf83171d617f37f428
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-02-06 00:22:35 +0200

    Solve 01.Sections task

--- DOM-ManipulationAndEvents/Exercise/01. Sections_Resources/01. Sections/script.js
+++ DOM-ManipulationAndEvents/Exercise/01. Sections_Resources/01. Sections/script.js
@@ -1,3 +1,24 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.getElementById('content');
+
+   for (const word of words) {
+      const divTag = document.createElement('div');
+
+      const pTag = document.createElement('p');
+      pTag.textContent = word;
+      pTag.style.display = 'none';
+
+      divTag.appendChild(pTag);
+      content.appendChild(divTag);
+
+      divTag.addEventListener('click', (e) => {
+         const pEl = e.target.querySelector('p');
+
+         if (pEl.style.display === 'none') {
+            pEl.style.display = '';
+         } else {
+            pEl.style.display = 'none';
+         }
+      });
+   }
 }
