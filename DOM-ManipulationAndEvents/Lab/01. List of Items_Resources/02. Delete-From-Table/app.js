337b9b822aa386a0857108703016d77bdb280fff
V-Mitev <vladimirmitev6969@gmail.com>
2026-02-05 13:37:30 +0200

    Solve 02.DeleteFromTable task

--- DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/02. Delete-From-Table/app.js
+++ DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/02. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const input = document.querySelector('input[name="email"]');
+
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    const rowsToDelete = rows.filter(row => row.children[1].textContent === input.value);
+
+    if (rowsToDelete.length > 0) {
+        rowsToDelete.forEach(row => row.remove());
+        document.getElementById('result').textContent = 'Deleted.';
+    } else {
+        document.getElementById('result').textContent = 'Not found.';
+    }
+
+    input.value = '';
 }
