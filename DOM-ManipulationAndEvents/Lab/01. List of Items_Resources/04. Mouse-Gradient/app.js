20c21c1d5babf6d473a52276ec84f41cbd9ead44
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-02-05 21:16:01 +0200

    Solve 04.MouseGradient task

--- DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/04. Mouse-Gradient/app.js
+++ DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/04. Mouse-Gradient/app.js
@@ -1,3 +1,14 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    const result = document.getElementById('result');
+
+    gradient.addEventListener('mousemove', (event) => {
+        const mouseX = event.offsetX;
+
+        const gradientWidth = event.target.clientWidth;
+
+        const percentage = (mouseX / gradientWidth) * 100;
+
+        result.textContent = Math.floor(percentage) + '%';
+    });
 }
