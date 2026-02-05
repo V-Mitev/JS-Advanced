9415647fbb0b6a0df6834438d1ee787c33c48478
V-Mitev <vladimirmitev6969@gmail.com>
2026-02-05 13:40:23 +0200

    Solve 02.DeleteFromTable task

--- DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/02. Delete-From-Table/app.js
+++ DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/02. Delete-From-Table/app.js
@@ -1,10 +1,9 @@
 function deleteByEmail() {
     const input = document.querySelector('input[name="email"]');
-    let inputValue = input.value;
-
+    
     const rows = Array.from(document.querySelectorAll('tbody tr'));
 
-    const rowsToDelete = rows.filter(row => row.children[1].textContent === inputValue);
+    const rowsToDelete = rows.filter(row => row.children[1].textContent === input.value);
 
     if (rowsToDelete.length > 0) {
         rowsToDelete.forEach(row => row.remove());
@@ -13,5 +12,5 @@ function deleteByEmail() {
         document.getElementById('result').textContent = 'Not found.';
     }
 
-    inputValue = '';
+    input.value = '';
 }
