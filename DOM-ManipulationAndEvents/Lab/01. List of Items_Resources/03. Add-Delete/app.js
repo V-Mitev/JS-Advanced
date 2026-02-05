cccec0cf0d239fc1686d14d6ee107bbfaf5b516f
V-Mitev <vladimirmitev6969@gmail.com>
2026-02-05 14:05:23 +0200

    Solve 03.Add/Delete task

--- DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/03. Add-Delete/app.js
+++ DOM-ManipulationAndEvents/Lab/01. List of Items_Resources/03. Add-Delete/app.js
@@ -1,3 +1,22 @@
 function addItem() {
-    //TODO...
+    const input = document.getElementById('newItemText');
+    const items = document.getElementById('items');
+
+    if(input.value !== '') {
+        const item = document.createElement('li');
+        item.textContent = input.value;
+        
+        const aTag = document.createElement('a');
+        aTag.textContent = '[Delete]';
+        aTag.href = '#';
+
+        aTag.addEventListener('click', function(e) {
+            e.target.parentElement.remove();
+        });
+
+        item.appendChild(aTag);
+        items.appendChild(item);
+        
+        input.value = '';
+    }
 }
