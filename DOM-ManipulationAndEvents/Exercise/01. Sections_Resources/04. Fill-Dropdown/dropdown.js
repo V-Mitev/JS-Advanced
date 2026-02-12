d7a11f3bc8d91282748da4c5efddbc851c383199
V-Mitev <vladimirmitev6969@gmail.com>
2026-02-12 16:46:14 +0200

    Solve 04.FillDropdown task

--- DOM-ManipulationAndEvents/Exercise/01. Sections_Resources/04. Fill-Dropdown/dropdown.js
+++ DOM-ManipulationAndEvents/Exercise/01. Sections_Resources/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    const menu = document.getElementById('menu');
+    const itemText = document.getElementById('newItemText');
+    const itemValue = document.getElementById('newItemValue');
+
+    const option = document.createElement('option');
+    option.value = itemValue.value;
+    option.textContent = itemText.value;
+
+    menu.appendChild(option);
+
+    itemText.value = '';
+    itemValue.value = '';
 }
