f913098a93618fd790c6a7d2baa5e884b7d71b90
V-Mitev <vladimirmitev6969@gmail.com>
2026-02-12 11:51:16 +0200

    Solve 03.LockedProfile task

--- DOM-ManipulationAndEvents/Exercise/01. Sections_Resources/03. Locked-Profile/app.js
+++ DOM-ManipulationAndEvents/Exercise/01. Sections_Resources/03. Locked-Profile/app.js
@@ -1,3 +1,19 @@
 function lockedProfile() {
-    console.log('TODO...')
+    document.getElementById('main').addEventListener('click', (e) => {
+        if(e.target.tagName === 'BUTTON') {
+            const button = e.target;
+            const profile = e.target.parentNode;
+            const lockBtn = profile.querySelector('input[type="radio"]');
+
+            if(lockBtn.checked) {
+                return;
+            }
+
+            button.textContent =  button.textContent === 'Show more' ? 'Hide it' : 'Show more';
+
+            const userInfo = profile.querySelector('div');
+
+            userInfo.style.display = userInfo.style.display === 'none' ? 'block' : 'none';
+        }
+    });
 }
