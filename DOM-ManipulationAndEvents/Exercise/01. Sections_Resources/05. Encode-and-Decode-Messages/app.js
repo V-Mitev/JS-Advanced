fbe8f1a9b90e8fee20e7ce99a77149b799e73078
V-Mitev <vladimirmitev6969@gmail.com>
2026-02-12 17:04:53 +0200

    Solve 05.EncodeAndDecodeMessages task

--- DOM-ManipulationAndEvents/Exercise/01. Sections_Resources/05. Encode-and-Decode-Messages/app.js
+++ DOM-ManipulationAndEvents/Exercise/01. Sections_Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,31 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const buttons = document.querySelectorAll('button');
+    const textAreas = document.querySelectorAll('textarea');
+
+    const encodeBtn = buttons[0];
+    const encodeTextArea = textAreas[0];
+
+    const decodeBtn = buttons[1];
+    const decodeTextArea = textAreas[1];
+
+    encodeBtn.addEventListener('click', (e) => {
+        const message = encodeTextArea.value;
+
+        const decodedMessage = message.split('')
+            .map(ch => String.fromCharCode(ch.charCodeAt(0) + 1))
+            .join('');
+
+        encodeTextArea.value = '';
+        decodeTextArea.value = decodedMessage;
+    });
+
+    decodeBtn.addEventListener('click', (e) => {
+        const message = decodeTextArea.value;
+
+        const decodedMessage = message.split('')
+            .map(ch => String.fromCharCode(ch.charCodeAt(0) - 1))
+            .join('');
+
+        decodeTextArea.value = decodedMessage;
+    });
 }
