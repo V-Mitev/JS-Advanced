23e61c68a2131d7d05b4dd769994e7c0942b3406
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-02-24 22:39:54 +0200

    Solve 07.SimpleCalculator task

--- Advanced Functions/Exercise/01. Sort Array_Resources/07. Simple Calculator/app.js
+++ Advanced Functions/Exercise/01. Sort Array_Resources/07. Simple Calculator/app.js
@@ -1,7 +1,28 @@
 function calculator() {
-    // TODO:
+    let element1;
+    let element2;
+    let resultElement;
+    debugger;
+    function init(selector1, selector2, resultSelector) {
+        element1 = document.querySelector(selector1);
+        element2 = document.querySelector(selector2);
+        resultElement = document.querySelector(resultSelector);
+    }
+
+    function add() {
+        debugger;
+        resultElement.value = Number(element1.value) + Number(element2.value);
+    }
+
+    function subtract() {
+        debugger;
+        resultElement.value = Number(element1.value) - Number(element2.value);
+    }
+
+    return {
+        init, add, subtract
+    };
 }
 
-
-
-
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result'); 
