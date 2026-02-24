a5aa1509e3d4ba607920195a760c02187de74c5e
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-02-24 22:55:11 +0200

    Solve 08.NextArticle task

--- Advanced Functions/Exercise/01. Sort Array_Resources/08. Next Article/script.js
+++ Advanced Functions/Exercise/01. Sort Array_Resources/08. Next Article/script.js
@@ -1,3 +1,19 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let index = 0;
+    const content = document.getElementById('content');
+
+    function addDivWithArticle() {
+        debugger;
+        if (index >= articles.length)
+            return;
+
+        const article = document.createElement('article');
+
+        article.textContent = articles[index];
+        index++;
+
+        content.appendChild(article);
+    }
+
+    return addDivWithArticle;
 }
